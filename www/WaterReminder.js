a9626d2e7a91d7070e3eb4f6ad8abccb10296c02
MAC <mac@MACs-MacBook-Pro.local>
2022-01-24 19:47:12 -0500

    project to app

--- www/WaterReminder.js
+++ www/WaterReminder.js
@@ -23,11 +23,10 @@
     function intervalStarter(){
     document.getElementById("glassCount").innerHTML = 0;
     glassCounter=0;
-    messageDrinkWater =  setInterval(showDrinkWaterMesssage ,1000);
-    glassCounterAdder = setInterval(glassCounterAdd,1000);
+    messageDrinkWater =  setInterval(showDrinkWaterMesssage ,2000);
+    glassCounterAdder = setInterval(glassCounterAdd,2000);
 
-    setTimeout(intervalStopper,6000);
-    document.getElementById("about").style.display = "none";
+    setTimeout(intervalStopper,11000);
 
     }
 
@@ -39,5 +38,4 @@
 
     function stopDay(){
         intervalStopper();
-        document.getElementById("about").style.display="block";
     }
